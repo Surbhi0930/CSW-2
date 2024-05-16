dbe69d5e8faa445878c016aa08a037e096fcc881
Surbhi0930 <147403159+Surbhi0930@users.noreply.github.com>
2024-05-16 16:21:13 +0530

    Update MostOccurringWords.java

--- Assignment_3/MostOccurringWords.java
+++ Assignment_3/MostOccurringWords.java
@@ -1,26 +1,19 @@
-package sem_4_Assignments.Assignment_3;
 
 import java.util.HashMap;
 
 public class MostOccurringWords {
     public static void findMostOccurringWords(String text) {
-        // Split the text into words
         String[] words = text.split("\\s+");
 
-        // Create a Hashtable to store word frequencies
         HashMap<String, Integer> wordFrequencyMap = new HashMap<>();
 
-        // Iterate through the words and update their frequencies in the Hashtable
         for (String word : words) {
-            // Remove any punctuation marks from the word
             word = word.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
-            // Update word frequency
             int frequency = wordFrequencyMap.getOrDefault(word, 0);
             wordFrequencyMap.put(word, frequency + 1);
         }
 
-        // Find the most occurring words
         int maxFrequency = 0;
         for (String word : wordFrequencyMap.keySet()) {
             int frequency = wordFrequencyMap.get(word);
