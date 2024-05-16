463996b288bfbc5bb9f2ed151fad27d784341724
Surbhi0930 <147403159+Surbhi0930@users.noreply.github.com>
2024-05-16 16:43:34 +0530

    Update q10.java

--- Strings/q10.java
+++ Strings/q10.java
@@ -1,17 +1,86 @@
 
-//String Rotation Checker: Write a program that checks if two strings are rotations of
-//each other (e.g., "apple" and "leapp"). Use methods like concat and indexOf to achieve
-//this.
-import java.util.*;
-public class q10 {
+import java.util.Scanner;
+
+public class Q10 {
+    static String Lower(String str) {
+        return str.toLowerCase();
+    }
+
+    static String Upper(String str) {
+        return str.toUpperCase();
+    }
+    static int searchChar(String str) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the character to search:");
+        char c = obj.next().charAt(0);
+        int firstIndex = str.indexOf(c);
+        System.out.println("First occurrence of char " + c + " is found at index : " + firstIndex);
+        return firstIndex;
+    }
+    static char index(String str) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter any Index:");
+        int i = obj.nextInt();
+        char result = str.charAt(i);
+        System.out.println("The Character found: " + result);
+        return result;
+    }
+    static String concatenate(String str) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter a new String to Concatenate:");
+        // Consume the newline character after taking input
+        String ne = obj.nextLine();
+        str = str.concat(ne);
+        return str;
+    }
+
+    static void display(String str) {
+        System.out.println("String: " + str);
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str1 = sc.next();
-        String str2 = sc.next();
-        String contactStr1 = str1+str1;
-        if(contactStr1.contains(str2)){
-            System.out.println(str2+" is a rotation of "+str1);
-            System.out.println("No of rotation:"+contactStr1.indexOf(str2));
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter any String:");
+        String str = obj.nextLine();
+        while (true) {
+            System.out.println("\nMenu:");
+            System.out.println("1. Lower Case");
+            System.out.println("2. Upper Case");
+            System.out.println("3. Search for a character");
+            System.out.println("4. Search for character at index");
+            System.out.println("5. Concatenate");
+            System.out.println("6. Display");
+            System.out.println("7. Exit");
+            System.out.println("Enter your choice:");
+
+            int choice = obj.nextInt();
+
+            switch (choice) {
+                case 1:
+                    str = Lower(str);
+                    break;
+                case 2:
+                    str = Upper(str);
+                    break;
+                case 3:
+                    searchChar(str);
+                    break;
+                case 4:
+                    index(str);
+                    break;
+                case 5:
+                    str = concatenate(str);
+                    break;
+                case 6:
+                    display(str);
+                    break;
+                case 7:
+                    System.out.println("Exiting...");
+                    obj.close();
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please enter a number from 1 to 7.");
+            }
         }
     }
 }
