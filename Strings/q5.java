04f3b9f516c0007d1c3b3a743146c70c6db6a222
Surbhi0930 <147403159+Surbhi0930@users.noreply.github.com>
2024-05-16 16:46:19 +0530

    Update q5.java

--- Strings/q5.java
+++ Strings/q5.java
@@ -1,47 +1,61 @@
-//Pig Latin Translator: Write a program that translates a sentence into Pig Latin. The first
-//consonant sound is moved to the end of the word followed by "ay". Use methods like
-//substring, concat, and loops for this translation.
-import java.util.*;
-public class q5 {
-    private static boolean isVowel(char c) {
-        return "aeiouAEIOU".indexOf(c) != -1;
-    }
-    private static String translateWordToPigLatin(String word) {
-        if (word.isEmpty()) return "";
+import java.util.Scanner;
 
-        // If the word starts with a consonant
-        if (!isVowel(word.charAt(0))) {
-            int index = 0;
-            // Finding the index of the first vowel
-            for (char c : word.toCharArray()) {
-                if (isVowel(c)) break;
-                index++;
-            }
-            return word.substring(index) + word.substring(0, index) + "ay";
-        } else {
-            // If the word starts with a vowel
-            return word + "ay";
+public class PerformanceTest {
+	private static StringBuilder BuilderConcatenation(String baseString, int numConcatenations) {
+        StringBuilder sb = new StringBuilder(baseString.length() * numConcatenations);
+        for (int i = 0; i < numConcatenations; i++) {
+            sb.append(baseString);
         }
+        return sb;
     }
 
-    public static String translateToPigLatin(String sentence) {
-        StringBuilder pigLatinSentence = new StringBuilder();
-        String[] words = sentence.split("\\s+");
-
-        for (String word : words) {
-            pigLatinSentence.append(translateWordToPigLatin(word)).append(" ");
+    private static StringBuffer BufferConcatenation(String baseString, int numConcatenations) {
+        StringBuffer stringBuffer = new StringBuffer(baseString.length() * numConcatenations);
+        for (int i = 0; i < numConcatenations; i++) {
+            stringBuffer.append(baseString);
         }
-
-        return pigLatinSentence.toString().trim();
+        return stringBuffer;
     }
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the base string: ");
+        System.out.print(">"+" ");
+        String baseString = scanner.nextLine();
+        System.out.println("Enter the number of concatenations: ");
+        System.out.print(">"+" ");
+        int numConcatenations = scanner.nextInt();
+        scanner.close();
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a sentence to translate into Pig Latin: ");
-        String sentence = sc.nextLine();
-        String pigLatinSentence = translateToPigLatin(sentence);
-        System.out.println("Pig Latin translation: " + pigLatinSentence);
+        long startTime, endTime, duration1,duration2;
 
-        sc.close();
+        // Using StringBuilder
+        startTime = System.currentTimeMillis();
+        StringBuilder stringBuilderResult = BuilderConcatenation(baseString, numConcatenations);
+        endTime = System.currentTimeMillis();
+        duration1 = endTime - startTime;
+        System.out.println("\nUsing StringBuilder...");
+        System.out.println("Time taken: " + duration1 + " milliseconds");
+        System.out.println("Final string length: " + stringBuilderResult.length());
+
+        // Using StringBuffer
+        startTime = System.currentTimeMillis();
+        StringBuffer stringBufferResult = BufferConcatenation(baseString, numConcatenations);
+        endTime = System.currentTimeMillis();
+        duration2 = endTime - startTime;
+        System.out.println("\nUsing StringBuffer...");
+        System.out.println("Time taken: " + duration2 + " milliseconds");
+        System.out.println("Final string length: " + stringBufferResult.length());
+        if(duration1>duration2)
+        {
+        	System.out.println("Comparison: StringBuffer was slower than StringBuilder by " +(duration2-duration1)+ " millisecond.");
+        }
+        else if(duration1<duration2)
+        {
+        	System.out.println("Comparison: StringBuilder was faster than StringBuffer by " +(duration2-duration1)+ " millisecond.");
+        }
+        else
+        	System.err.println("Both are same time!!!!");
     }
+
+    
 }
