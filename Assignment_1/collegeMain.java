19d19bd7a1e15e72321a1a442f128301ff5b542d
Surbhi0930 <147403159+Surbhi0930@users.noreply.github.com>
2024-05-16 16:07:28 +0530

    q5.java

--- Assignment_1/collegeMain.java
+++ Assignment_1/collegeMain.java
@@ -1,22 +1,9 @@
-//Q5. Developing a Simple College Management System in Java
-//Create a Java program for managing colleges and students. The provided classes model the
-//relationship between colleges and students.
-//The College class represents a college with attributes collegeName and collegeLoc. The
-//Student class represents a student with attributes studentId, studentName, and a reference
-//        to a College object. Enhance the Student class by adding a constructor that assigns a student
-//        ID, name, and college information. Additionally, add a method named displayStudentInfo()
-//that prints the student's ID, name, and the college information in which they are enrolled.
-//In the Main class, instantiate at least two College objects and at least two Student objects.
-//Enroll each student in one of the colleges. Then, display the information of all colleges and all
-//students using the appropriate methods.
-package sem_4_Assignments.assignment_1;
-
-public class collegeMain {
+public class q5 {
     public static void main(String[] args) {
         college c1 = new college("Iter","Jagamara");
         college c2 = new college("Kiit","Patia");
-        student s1 = new student(c1.getCollegeName(), c1.getCollegeLoc(), "12345678","Ayushmaan");
-        student s2 = new student(c2.getCollegeName(), c2.getCollegeLoc(), "12345678","Ayushmaan");
+        student s1 = new student(c1.getCollegeName(), c1.getCollegeLoc(), "12345678","Surbhi");
+        student s2 = new student(c2.getCollegeName(), c2.getCollegeLoc(), "12345678","Rajveer");
         s1.display();
         s2.display();
     }
