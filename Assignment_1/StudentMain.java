f3d022ad301fe21465ffdc559fc0f175f1690e6f
Surbhi0930 <147403159+Surbhi0930@users.noreply.github.com>
2024-05-16 16:02:52 +0530

    Update StudentMain.java

--- Assignment_1/StudentMain.java
+++ Assignment_1/StudentMain.java
@@ -120,7 +120,7 @@ class Enrollment implements EnrollmentSystem {
     }
 }
 
-public class StudentMain {
+public class 10 {
     public static void main(String[] args) {
         Student student1 = new Student("1001", "Alice");
         Student student2 = new Student("1002", "Bob");
