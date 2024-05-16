71417853fe0e8ca439c169a81e1839c760749929
Surbhi0930 <147403159+Surbhi0930@users.noreply.github.com>
2024-05-16 16:15:01 +0530

    Update CarDriver.java

--- Assignment_2/CarDriver.java
+++ Assignment_2/CarDriver.java
@@ -1,10 +1,3 @@
-//Write a program to create a Car class with member variables model, color,
-//        and speed. Add the respective method and constructor to it. Create a
-//driver class in that class and create two car objects. Compare the car
-//objects according to their speed and print the details of the car that has
-//a greater speed.
-//        Note: Overload compareTo method of Comparable interface.
-package sem_4_Assignments.Assignment_2;
 
 class Car {
     private String model;
@@ -36,19 +29,15 @@ class Car {
 
 public class CarDriver{
     public static void main(String[] args) {
-        // Create two car objects
         Car car1 = new Car("Toyota Camry", "Blue", 120.5);
         Car car2 = new Car("Honda Civic", "Red", 115.8);
 
-        // Compare car speeds
         if (car1.isFasterThan(car2)) {
-            System.out.println(car1.getModel() + " is faster than " + car2.getModel());
             System.out.println("Details of the faster car:");
             System.out.println("Model: " + car1.getModel());
             System.out.println("Color: " + car1.getColor());
             System.out.println("Speed: " + car1.getSpeed() + " km/h");
         } else {
-            System.out.println(car2.getModel() + " is faster than " + car1.getModel());
             System.out.println("Details of the faster car:");
             System.out.println("Model: " + car2.getModel());
             System.out.println("Color: " + car2.getColor());
