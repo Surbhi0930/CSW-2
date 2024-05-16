37b870938845bb2892600af8cf1e67d3171f455c
Surbhi0930 <147403159+Surbhi0930@users.noreply.github.com>
2024-05-16 16:10:53 +0530

    Update shape.java

--- Assignment_1/shape.java
+++ Assignment_1/shape.java
@@ -1,46 +1,43 @@
-//. Design a Java class called Rectangle with private attributes length and width. Include a
-//constructor to initialize these attributes, as well as setter and getter methods for each attribute.
-//Additionally, implement methods to calculate the area and perimeter of the rectangle. Write a
-//main method to create an object of the Rectangle class, set values for its attributes, and display
-//the area and perimeter.
-package sem_4_Assignments.assignment_1;
 
-public class shape {
-    public static void main(String[] args) {
-        rectangle r = new rectangle(0,0);
-        r.setLength(12);
-        r.setWidth(20);
-        System.out.println(r.getLength()+" "+r.getWidth());
-        System.out.println(r.area()+" "+r.perimeter());
-    }
+public class q2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Rectangle rectangle=new Rectangle(2,6);
+System.out.println(rectangle.displayArea());
+System.out.println(rectangle.displayPerimeter());
+rectangle.setlength(5);
+rectangle.setwidth(10);
+System.out.println(rectangle.displayArea());
+System.out.println(rectangle.displayPerimeter());
+
+	}
+
 }
-class rectangle{
-    private double length;
-    private double width;
-    rectangle(double length,double width){
-        this.length = length;
-        this.width = width;
-    }
+class Rectangle{
+	int length;
+	int width;
+	Rectangle(int length,int width){
+		this.length=length;
+		this.width=width;
+	}
+	public int getlength() {
+		return this.length;
+	}
+	public void setlength(int length) {
+		this.length=length;
+	}
+	public int getwidth() {
+		return this.width;
+	}
+	public void setwidth(int width) {
+		this.width=width;
+	}
+	public double displayArea() {
+		return length*width;
+	}
+	public double displayPerimeter() {
+		return 2*(length+width);
+	}
 
-    public double getLength() {
-        return length;
-    }
-
-    public void setLength(double length) {
-        this.length = length;
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public void setWidth(double width) {
-        this.width = width;
-    }
-    double area(){
-        return length*width;
-    }
-    double perimeter(){
-        return 2*(length+width);
-    }
 }
