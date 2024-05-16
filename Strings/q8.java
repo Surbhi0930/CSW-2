642201e41f533a3a24258839b521564374344815
Surbhi0930 <147403159+Surbhi0930@users.noreply.github.com>
2024-05-16 16:48:40 +0530

    Update q8.java

--- Strings/q8.java
+++ Strings/q8.java
@@ -1,18 +1,24 @@
-//Caesar Cipher Encryption/Decryption: Write a program that implements the Caesar
-//cipher for encryption and decryption. Use methods like toCharArray and loops to shift
-//characters by a specific key value.
-import java.util.*;
+
+import java.util.Scanner;
+
 public class q8 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str1 = sc.next();
-        String str2 = "";
-        int x = sc.nextInt();
-        char[] ch = str1.toCharArray();
-        for (int i=0;i<ch.length;i++){
-            ch[i] = (char) (ch[i]+x);
-            str2 +=ch[i];
-        }
-        System.out.println(str2);
-    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner obj = new Scanner(System.in);
+        System.out.println("Enter 1st String:");
+        String a = obj.nextLine();
+        String d=a;
+        System.out.println("Enter 2nd String:");
+        String b = obj.nextLine();
+        a=a.concat(b);
+        System.out.println("After using String Method the String is: "+a);
+        System.out.println("After using using '+' operator the String is: "+(d+b));
+        System.out.println("Enter any index to see the Character:");
+        int index=obj.nextInt();
+        char result=a.charAt(index);
+        System.out.println("The Character found: "+result);
+
+	}
+
 }
